3000c97a2989186fe583fca7b5b3af3457075770
PADO <62028267+47PADO47@users.noreply.github.com>
2023-06-15 20:50:41 +0200

    ♻️ Clean MX2 class

--- src/core/MX2.ts
+++ src/core/MX2.ts
@@ -1,12 +1,11 @@
 import { AbstractConstructorOptions, FetchOptions, FetchResponse } from "@/types/base";
 import Base from "./base";
-import { fetch } from "undici";
 
 class MX2 extends Base {
     constructor(options: AbstractConstructorOptions) {
         super({
             ...options,
-            gameVersion: 2,
+            game: 'madskillsmotocross2',
         });
     }
     
@@ -16,26 +15,6 @@ class MX2 extends Base {
         });
         return data.serverTime;
     };
-
-    async fetch<T>(options: FetchOptions): Promise<T & FetchResponse> {
-
-        this.log(`fetch (/${options.path}) - ${this.authenticated ? 'User 🔐' : 'Guest 🌐'}`);
-
-        const json = this.encodeJson(this.mergeJson(options.body || {}));
-        const response = await fetch(`${this.baseUrl}${options.path}`, {
-            headers: this.headers,
-            body: `json=${json}`,
-            method: 'POST',
-        });
-
-        const data = await response.json()
-            .catch(() => this.error('could not parse json')) as T & FetchResponse;
-
-        if (data.result !== 'SUCCESS' || data.errorMessage.length > 0) return this.error(`${data.errorMessage} (${data.result})`);
-        if (!response.ok) return this.error(`Response not ok (${response.status} - ${response.statusText})`);
-
-        return data;
-    }
 }
 
 export default MX2;
