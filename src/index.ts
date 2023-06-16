92305bce56f363b907f9ddf60eda5fd1dceb5e2b
PADO <62028267+47PADO47@users.noreply.github.com>
2023-06-16 16:08:55 +0200

    🩹 Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export { default as MX2 } from '@/core/mx2';
-export { default as BMX2 } from '@/core/bmx2';
+export { default as MX2 } from './core/mx2';
+export { default as BMX2 } from './core/bmx2';
