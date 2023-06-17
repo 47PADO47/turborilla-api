a155dc77b09f4b3dba39e7efc76c53131b09b1ee
PADO <62028267+47PADO47@users.noreply.github.com>
2023-06-17 12:37:41 +0200

    ✨ Extend MX2#getUserDataMappings()

--- src/core/MX2.ts
+++ src/core/MX2.ts
@@ -1,5 +1,6 @@
 import { AbstractConstructorOptions } from "@/types/base";
 import Base from "./base";
+import { getUserDataOptsMX2 } from "../types/MX2";
 
 class MX2 extends Base {
     constructor(options: AbstractConstructorOptions) {
@@ -8,6 +9,13 @@ class MX2 extends Base {
             game: 'madskillsmotocross2',
         });
     };
+
+    getUserDataMappings() {
+        return {
+            ...super.getUserDataMappings(),
+            dailyDash: 'dailydash',
+        }
+    }
 }
 
 export default MX2;
